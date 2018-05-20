76b4a6df1de211fbc700839e3206379979c64e0a
unknown <uptimevic@gmail.com>
2018-05-20 09:56:39 -0400

    kick

--- helloAppS/assets/js/setup.js
+++ helloAppS/assets/js/setup.js
@@ -1,4 +1,45 @@
+// http://github.com/logzio/logzio-js
+(function(window) {
+	const LogzioLogger = function(apiKey, sendConsoleJsErrors) {
+		this.key = apiKey
+		if (sendConsoleJsErrors) sendConsoleErrors()
+	}
+  let sendConsoleErrors = function() {
+	  window.onerror = function (msg, url, line, col) {
+			LogzioLogger.log({
+				message: msg,
+				url: url,
+				line: line,
+				col: col
+			})
+		}
+	}
+	LogzioLogger.prototype.log = function(data) {
+		 try {
+			let parsedMsg = typeof data == 'object' ? data : { message:data }
+			let logUrl = window.location.protocol + '//listener.logz.io:'
+			logUrl += (window.location.protocol === 'http:' ? '8090' : '8091') + '?token=' + this.key
+			Object.keys(parsedMsg).forEach(function(key) {
+				logUrl += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(parsedMsg[key])
+			})
+			let logImg = new Image()
+			logImg.src = logUrl
+		 } catch (ex) {
+			if (window && window.console && typeof window.console.log == 'function')
+				console.log("Failed to send log because of exception:\n" + ex)
+		 }
+	}
+	window.LogzioLogger = LogzioLogger
+})(window)
 
+window.log = new LogzioLogger('__YOUR_API_TOKEN__')
+
+log.log('Hello, this is just a test')
+log.log({hello: 'there',
+			test: 'hello world'
+})
+
+// ////////////////////////////////////////////////////////////////////////////
  // http://github.com/muicss/loadjs/issues/56
 
  // https://jsfiddle.net/muicss/4791kt3w
